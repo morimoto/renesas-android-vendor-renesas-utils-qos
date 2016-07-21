3c48e17695ddc27a1c0f9dad82fb8e5275116e20
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com.ua>
2016-07-21 14:53:13 +0300

    Android.mk: Aded build rules for test application
    Change-Id: I3d43681f2eb23820d2aaab7b247ced7499445fac
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com.ua>

--- Android.mk
+++ Android.mk
@@ -11,3 +11,16 @@ LOCAL_SRC_FILES := qos_if-module/files/qos_if/if/qos_lib.c
 LOCAL_MODULE:= libqos
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES:= \
+    $(LOCAL_PATH)/qos_if-module/files/qos_if/include
+
+LOCAL_MODULE := qos_tp
+LOCAL_SHARED_LIBRARIES := libqos
+
+LOCAL_SRC_FILES := \
+    qos_if-tp-user/files/qos_if/qos_tp.c
+
+include $(BUILD_EXECUTABLE)
