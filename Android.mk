bc327fcbc0d9cf1b3faf25eb3142d1afead56bfc
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2017-01-17 17:06:30 +0200

    Android.mk: QOS library and test tool moved to VENDOR partition.
    Also enabled only 64 bits target.
    
    Change-Id: Ie0cfd558282f743b5ef1e58c01eb8ac519c10850
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>

--- Android.mk
+++ Android.mk
@@ -8,7 +8,10 @@ LOCAL_C_INCLUDES:= $(LOCAL_PATH)/qos_if-module/files/qos_if/include
 
 LOCAL_SRC_FILES := qos_if-module/files/qos_if/if/qos_lib.c
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE:= libqos
+
+LOCAL_MULTILIB := 64
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -17,10 +20,13 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/qos_if-module/files/qos_if/include
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 LOCAL_MODULE := qos_tp
 LOCAL_SHARED_LIBRARIES := libqos
 
 LOCAL_SRC_FILES := \
     qos_if-tp-user/files/qos_if/qos_tp.c
+
+LOCAL_MULTILIB := 64
 
 include $(BUILD_EXECUTABLE)
