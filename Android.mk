4cc6917b76fbabf7bea172ddc7f745ceb5009a08
Renesas Buildbot <renesas.buildbot@globallogic.com>
2019-05-08 15:11:44 +0300

    Workarounds for Android Q

--- Android.mk
+++ Android.mk
@@ -5,11 +5,11 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_C_INCLUDES:= $(LOCAL_PATH)/qos_if-module/files/qos_if/include \
-                   $(LOCAL_PATH)/../qos_km/qos-module/files/qos/drv
+                   $(TOP)/hardware/renesas/modules/qos/qos-module/files/qos/drv
 
 LOCAL_SRC_FILES := qos_if-module/files/qos_if/if/qos_lib.c
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE:= libqos
 
 LOCAL_MULTILIB := 64
@@ -20,9 +20,9 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/qos_if-module/files/qos_if/include \
-    $(LOCAL_PATH)/../qos_km/qos-module/files/qos/drv
+    $(LOCAL_PATH)/hardware/renesas/modules/qos/qos-module/files/qos/drv
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE := qos_tp
 LOCAL_SHARED_LIBRARIES := libqos
 
